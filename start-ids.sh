ab5ecd8b6d65b03ce795faefb6719c4871ac8c3b
sadamalsharabi <44142745+sadamshr3be@users.noreply.github.com>
2024-11-04 14:20:19 +0300

    Add files via upload

---
+++ start-ids.sh
@@ -0,0 +1,21 @@
+apt install pv -y
+
+apt install figlet -y
+
+termux-storage-setup -y
+
+apt install ruby -y
+apt install python -y
+apt install php -y
+gem install lolcat
+
+apt install p7zip -y
+xdg-open https://t.me/termuxalsharabi
+7z x ids-inf.7z -pnidhin123
+cd ids-inf
+
+
+7z x ids_tool.7z -pids123
+cd ids_tool
+cd ids_tool
+bash ids3.sh
